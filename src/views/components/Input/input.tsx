59d2501ecf676f72330cb6ee325baa295529df46
katerina <eolejnik@icloud.com>
2023-09-10 11:13:46 +0200

    сделана логика и стилизация компонента добавления задачи

--- src/views/components/Input/input.tsx
+++ src/views/components/Input/input.tsx
@@ -1,13 +1,36 @@
-import React from 'react'
-
+import React, { useCallback, useState } from 'react'
+import styles from './input.module.scss'
+import { on } from 'stream'
 interface InputProps {
   onAdd: (text: string) => void
 }
 
 export const Input: React.FC<InputProps> = ({ onAdd }) => {
+  const [inputValue, setInputValue] = useState('')
+  const addTask = useCallback(() => {
+    onAdd(inputValue)
+    setInputValue('')
+  }, [inputValue])
+
   return (
-    <>
-      <p>123</p>
-    </>
+    <div className={styles.input}>
+      <input
+        type="text"
+        className={styles.inputValue}
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+        onKeyDown={(e) => {
+          if (e.key === 'Enter') {
+            addTask()
+          }
+        }}
+        placeholder="Type here..."
+      />
+      <button
+        onClick={addTask}
+        aria-label="Add"
+        className={styles.inputButton}
+      />
+    </div>
   )
 }
